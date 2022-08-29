1d55bc624de3a5568f6fb2ac6fd9857b600be0c2
nathanhall762 <4943@holbertonstudents.com>
2022-08-29 12:34:10 -0500

    fix: zero to 0

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -13,7 +13,7 @@ if number >= 0:
     if last > 5:
         print("Last digit of {} is {} and is greater than 5".format(number, last))
     elif last == 0:
-        print("Last digit of {} is {} and is zero".format(number, last))
+        print("Last digit of {} is {} and is 0".format(number, last))
     else:
         print("Last digit of {} is {} and is less than 6 and not 0".format(number, last))
 if number < 0:
