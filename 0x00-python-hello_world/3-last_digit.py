c05c4d609dff22ab779cf84797e66af244f7664c
nathanhall762 <4943@holbertonstudents.com>
2022-08-29 12:29:42 -0500

    fix: did arithmetic in variable

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# YOUR CODE HERE
+last = 0
 if number > 0:
-    if number % 10 > 5:
-        print("Last digit of {} is {} and is greater than 5".format(number, number % 10))
-    elif number % 10 == 0:
-        print("Last digit of {} is {} and is zero".format(number, number % 10))
+    last = number % 10
+elif number <= 0:
+    last = (-1 * number) % 10
+
+if number > 0:
+    if last > 5:
+        print("Last digit of {} is {} and is greater than 5".format(number, last))
+    elif last == 0:
+        print("Last digit of {} is {} and is zero".format(number, last))
     else:
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number, number % 10))
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number, last))
 if number < 0:
-    if (-1 * number) % 10 > 5:
-        print("Last digit of {} is -{} and is greater than 5".format(number, (-1 * number) % 10))
-    elif (-1 * number) % 10 == 0:
-        print("Last digit of {} is -{} and is zero".format(number, (-1 * number) % 10))
+    if last > 5:
+        print("Last digit of {} is -{} and is greater than 5".format(number, last))
+    elif last == 0:
+        print("Last digit of {} is -{} and is zero".format(number, last))
     else:
-        print("Last digit of {} is -{} and is less than 6 and not 0".format(number, (-1 * number) % 10))
+        print("Last digit of {} is -{} and is less than 6 and not 0".format(number, last))
