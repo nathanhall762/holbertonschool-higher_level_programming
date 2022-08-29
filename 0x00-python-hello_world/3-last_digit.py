da9d60c9aedcb27a99cf7c2bffa86b5c624bccc8
nathanhall762 <4943@holbertonstudents.com>
2022-08-29 12:19:13 -0500

    fix: sign for negatives

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -11,8 +11,8 @@ if number > 0:
         print("Last digit of {} is {} and is less than 6 and not 0".format(number, number % 10))
 if number < 0:
     if (-1 * number) % 10 > 5:
-        print("Last digit of {} is {} and is greater than 5".format(number, (-1 * number) % 10))
+        print("Last digit of {} is -{} and is greater than 5".format(number, (-1 * number) % 10))
     elif (-1 * number) % 10 == 0:
-        print("Last digit of {} is {} and is zero".format(number, (-1 * number) % 10))
+        print("Last digit of {} is -{} and is zero".format(number, (-1 * number) % 10))
     else:
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number, (-1 * number) % 10))
+        print("Last digit of {} is -{} and is less than 6 and not 0".format(number, (-1 * number) % 10))
