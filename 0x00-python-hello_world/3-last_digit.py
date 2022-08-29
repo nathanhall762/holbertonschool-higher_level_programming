ef0629ccd7e33827155970d007aac04795750bfb
nathanhall762 <4943@holbertonstudents.com>
2022-08-29 12:31:22 -0500

    fix: last negation in if

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -15,9 +15,10 @@ if number > 0:
     else:
         print("Last digit of {} is {} and is less than 6 and not 0".format(number, last))
 if number < 0:
+    last = last * -1
     if last > 5:
-        print("Last digit of {} is -{} and is greater than 5".format(number, last))
+        print("Last digit of {} is {} and is greater than 5".format(number, last))
     elif last == 0:
-        print("Last digit of {} is -{} and is zero".format(number, last))
+        print("Last digit of {} is {} and is zero".format(number, last))
     else:
-        print("Last digit of {} is -{} and is less than 6 and not 0".format(number, last))
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number, last))
