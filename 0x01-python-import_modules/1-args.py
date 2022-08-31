98f00df0f4aa14d5a8c0344e232c0a8170077c3d
nathanhall762 <4943@holbertonstudents.com>
2022-08-31 11:17:55 -0500

    fix: conditional for argv 1 and 2

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -2,6 +2,11 @@
 from sys import argv
 
 __name__ == '__main__'
-print("{} arguments:".format(len(argv) - 1))
+if len(argv) == 1:
+    print("{} arguments.".format(len(argv) - 1))
+elif len(argv) == 2:
+    print("{} argument:".format(len(argv) - 1))
+else:
+    print("{} arguments:".format(len(argv) - 1))
 for i in range(1, len(argv)):
     print("{}: {}".format(i, argv[i]))
