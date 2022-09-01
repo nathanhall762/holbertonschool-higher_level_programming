606b5abd98cc09ac2db241c07d1a9a0be2af3f72
nathanhall762 <4943@holbertonstudents.com>
2022-09-01 15:00:28 -0500

    fix: copied list

--- 0x02-python-data_structures/4-new_in_list.py
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -4,6 +4,6 @@ def new_in_list(my_list, idx, element):
         return my_list
     if idx > len(my_list):
         return my_list
-    copy_list = my_list
+    copy_list = my_list[:]
     copy_list[idx] = element
     return copy_list
