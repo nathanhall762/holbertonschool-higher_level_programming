86d79185c9be8ed13b1ea26a7b56308eaf613231
nathanhall762 <4943@holbertonstudents.com>
2022-09-02 10:18:26 -0500

    fix: file 6 fix

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
-	for j in range(0, 10):
-		if (i!=j):
-			print("{}{}".format(i, j), end ='')
-			if (i!=8 or j!=9):
-				print(", ", end ='')
-print(end ='\n')
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}, ".format(i, j), end='')
