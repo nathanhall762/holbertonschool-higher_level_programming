c5f6ec1536af011aff39b596d42c45401cd61bb6
nathanhall762 <4943@holbertonstudents.com>
2022-09-07 14:29:25 -0500

    init: task 3

---
+++ 0x04-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Square Class
+
+This is a square
+
+"""
+
+
+class Square:
+    """Summary of class here.    """
+    def __init__(self, size=0):
+        """Inits SampleClass with blah."""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """does a thing"""
+        return self.__size ** 2
