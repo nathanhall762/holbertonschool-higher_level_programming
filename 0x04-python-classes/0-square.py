651234e1e5b238fe5d008cd78d8a2bd6d492f876
nathanhall762 <4943@holbertonstudents.com>
2022-09-07 11:55:38 -0500

    style: doc

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -6,13 +6,19 @@ This is a square
 """
 
 class Square:
-        """__init__
-        
-        Initializes the size of the square.
-        
-        Attributes:
-            size (int): size of square
-        
-        """
     def __init__(self, size):
+        """Example of docstring on the __init__ method.
+
+        The __init__ method may be documented in either the class level
+        docstring, or as a docstring on the __init__ method itself.
+
+        Either form is acceptable, but the two should not be mixed. Choose one
+        convention to document the __init__ method and be consistent with it.
+
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+
+        Args:
+            param1 (str): Description of `param1`.
+        """
         self.__size = size #: doc comment inline
