49923dc2bcb399f9c51cfda7708259212d98c407
nathanhall762 <4943@holbertonstudents.com>
2022-09-07 11:50:47 -0500

    style: doc check

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -7,12 +7,12 @@ This is a square
 
 class Square:
     def __init__(self, size):
-        """Initializes size __init__
+        """__init__
         
         Initializes the size of the square.
         
-        Args:
-            size (int): size of square.
+        Attributes:
+            size (int): size of square
         
         """
         self.__size = size
