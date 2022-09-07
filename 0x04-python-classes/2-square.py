f56b62b829294d34037046cb2e9d066d84e137d0
nathanhall762 <4943@holbertonstudents.com>
2022-09-07 12:54:59 -0500

    init" task 2

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -22,3 +22,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """does a thing"""
+        return self.__size ** 2
