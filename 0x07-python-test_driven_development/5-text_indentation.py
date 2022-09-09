10970c54501d21f49bda82cba80b5f99bf48fbc4
nathanhall762 <4943@holbertonstudents.com>
2022-09-09 15:43:23 -0500

    fix: starting variable for 5 flow

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,15 +3,15 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     line_list = []
+    starting = True
     for idx in range(0, len(text)):
+        if text[idx] == " " and starting is True:
+            idx += 1
+            continue
         if text[idx] == "." or text[idx] == "?" or text[idx] == ":":
-            #print("{}\n".format(text[idx]))
-            line_list.append(text[idx])
-            print(*line_list, sep='')
-            line_list = []
+            print("{}\n".format(text[idx]))
+            starting = True
         else:
-            #print(text[idx], end='')
-            line_list.append(text[idx])
+            print(text[idx], end='')
+            starting = False
     print("\n")
-
-#this shit is totally fucked. redo logic because space at beginning of line is a nogo.
