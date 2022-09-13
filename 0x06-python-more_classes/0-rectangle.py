3f9c1b24238c7a422e555a1fe0abca2cd13d87a2
nathanhall762 <4943@holbertonstudents.com>
2022-09-13 10:25:26 -0500

    init: task 0

---
+++ 0x06-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    def height(self):
+        return self.height
+
+    def height(self, value):
+        self.height = value
+
+    def width(self):
+        return self.width
+
+    def width(self, value):
+        self.width = value
