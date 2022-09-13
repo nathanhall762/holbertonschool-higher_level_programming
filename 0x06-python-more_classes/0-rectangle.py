7143e9f3ab694629fe0ab5e1b915a46d64b41bea
nathanhall762 <4943@holbertonstudents.com>
2022-09-13 11:39:34 -0500

    fix: if check order 0

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -14,9 +14,9 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if value < 0:
-            raise ValueError("height must be >= 0")
         if isinstance(value, int):
+            if value < 0:
+                raise ValueError("height must be >= 0")
             self._height = value
         else:
             raise TypeError("height must be an integer")
@@ -27,9 +27,9 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if value < 0:
-            raise ValueError("width must be >= 0")
         if isinstance(value, int):
+            if value < 0:
+                raise ValueError("width must be >= 0")
             self._width = value
         else:
             raise TypeError("width must be an integer")
