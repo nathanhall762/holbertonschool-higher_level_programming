d92adef2d764b6dbdc132787077615f36434b220
nathanhall762 <4943@holbertonstudents.com>
2022-09-16 10:49:07 -0500

    init: task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" documentation """
+BaseGeometry = __import__('9-rectangle').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        Rectangle(size, size)
