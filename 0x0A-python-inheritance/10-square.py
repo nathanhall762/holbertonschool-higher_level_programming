72e7df3534470d92896a649e2ec30fa88933b3e5
nathanhall762 <4943@holbertonstudents.com>
2022-09-16 10:49:46 -0500

    fix: style

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,6 +5,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """ documentation """
     def __init__(self, size):
         self.integer_validator("size", size)
         Rectangle(size, size)
