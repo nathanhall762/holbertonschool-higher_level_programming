561130e561dc42b30a5e3917b33de540be1b2de3
nathanhall762 <4943@holbertonstudents.com>
2022-09-19 11:42:42 -0500

    fix: utf-8 to utf8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """ documentation """
-    with open(filename, encoding="utf-8") as myFile:
+    with open(filename, encoding="utf8") as myFile:
         print(myFile.read())
