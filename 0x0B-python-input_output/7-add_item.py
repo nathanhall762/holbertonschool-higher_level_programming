3737b5fadbb269629b0d222f10af9d4264d5614c
nathanhall762 <4943@holbertonstudents.com>
2022-09-19 20:18:38 -0500

    fix: unbound variable wrangling

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import os
-import sys
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py')\
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file')\
     .load_from_json_file
 
-try:
-    loadFile = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    loadfile = []
+if os.path.exists('add_item.json'):
+    loadFile = load_from_json_file('add_item.json')
+else:
+    loadFile = []
 
 for idx in range(1, len(argv)):
-    loadFile.append(argv[i])
+    loadFile.append(argv[idx])
 
 save_to_json_file(loadFile, 'add_item.json')
