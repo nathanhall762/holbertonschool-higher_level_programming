b2688ab3a54c2a2f25aa47a0b527adc94e085181
nathanhall762 <4943@holbertonstudents.com>
2022-09-19 20:09:42 -0500

    init: task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import os
+import sys
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file.py')\
+    .load_from_json_file
+
+try:
+    loadFile = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    loadfile = []
+
+for idx in range(1, len(argv)):
+    loadFile.append(argv[i])
+
+save_to_json_file(loadFile, 'add_item.json')
