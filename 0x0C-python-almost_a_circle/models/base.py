18d302b3d18c53f33f6d1936830e411e8d5bcb2d
nathanhall762 <4943@holbertonstudents.com>
2022-09-23 10:02:23 -0500

    init: task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,7 +15,20 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
-        """ returns the JSON string representation of input """
+        """ returns the JSON string representation of list_dictionaries """
+
         if list_dictionaries is None:
             return []
         return json.dumps(list_dictionaries)
+
+    def save_to_file(cls, list_objs):
+        """ writes the JSON string representation of list_objs to a file """
+
+        filename = cls.__name__ + '.json'
+        with open(filename, mode='w') as f:
+            if list_objs is None:
+                return f.write(cls.to_json_string(None))
+            json_add = []
+            for element in list_objs:
+                json_add.append(element.to_dictionary())
+            return f.write(cls.to_json_string(json_add))
