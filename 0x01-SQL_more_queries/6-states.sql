c851173e119b9c489ddaa4d870f1a7aec63b5e91
nathanhall762 <4943@holbertonstudents.com>
2022-10-06 13:33:33 -0500

    try 6: move Primary Key line

--- 0x01-SQL_more_queries/6-states.sql
+++ 0x01-SQL_more_queries/6-states.sql
@@ -2,7 +2,6 @@
 -- table states (in the database hbtn_0d_usa) 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
-	id INT UNIQUE NOT NULL,
-	PRIMARY KEY (id),
+	id INT UNIQUE NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 )
