86196d48ef6c26557afbf791ef34642b1ec318b9
nathanhall762 <4943@holbertonstudents.com>
2022-10-06 14:02:38 -0500

    init 8:

---
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+--  lists all the cities of California that
+-- can be found in the database hbtn_0d_usa
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.id = state.id AND state.name = 'California'
+ORDER BY cities.id ASC;
