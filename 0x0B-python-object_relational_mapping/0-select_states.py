8bca524cdf5b5cb0485520638fd28513cd124066
nathanhall762 <4943@holbertonstudents.com>
2022-10-18 12:36:50 -0500

    fix: added if __name...

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import MySQLdb
 
-db = MySQLdb.connect(user="root", passwd="usaUSA777", db="hbtn_0e_0_usa")
-c = db.cursor()
-c.execute("""SELECT id name FROM states ORDER BY id ASC""")
-records = c.fetchall()
-for record in records:
-    print(record)
+if __name__ == '__main__':
+    db = MySQLdb.connect(user="root", passwd="usaUSA777", db="hbtn_0e_0_usa")
+    c = db.cursor()
+    c.execute("""SELECT id name FROM states ORDER BY id ASC""")
+    records = c.fetchall()
+    for record in records:
+        print(record)
