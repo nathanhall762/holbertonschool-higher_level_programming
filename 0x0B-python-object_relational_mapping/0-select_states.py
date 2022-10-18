79c18839f38939a5b977cf846da93a212487d724
nathanhall762 <4943@holbertonstudents.com>
2022-10-18 12:33:31 -0500

    init 0:

---
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import MySQLdb
+
+db = MySQLdb.connect(user="root", passwd="usaUSA777", db="hbtn_0e_0_usa")
+c = db.cursor()
+c.execute("""SELECT id name FROM states ORDER BY id ASC""")
+records = c.fetchall()
+for record in records:
+    print(record)
