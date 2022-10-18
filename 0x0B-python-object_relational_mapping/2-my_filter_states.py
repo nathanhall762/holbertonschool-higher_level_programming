2c945460c817724042adfc0d0183b2d8967cbdbd
nathanhall762 <4943@holbertonstudents.com>
2022-10-18 13:05:19 -0500

    style 2

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -6,6 +6,7 @@ from sys import argv
 
 if __name__ == '__main__':
     """documentation"""
+
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
     state = argv[4]
