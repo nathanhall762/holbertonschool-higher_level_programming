a8c53fa1f2df094f55dc861be22dfffccf0c8251
nathanhall762 <4943@holbertonstudents.com>
2022-10-18 12:47:16 -0500

    init 1

---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" documentation """
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """documentation"""
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    records = c.fetchall()
+    for record in records:
+        print(record)
