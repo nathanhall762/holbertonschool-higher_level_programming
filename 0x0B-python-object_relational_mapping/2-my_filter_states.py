2f288ad105b2f86c6328c1b3cb06813b4d279bf9
nathanhall762 <4943@holbertonstudents.com>
2022-10-18 13:10:57 -0500

    fix 2: used format

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -6,12 +6,11 @@ from sys import argv
 
 if __name__ == '__main__':
     """documentation"""
-
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
     state = argv[4]
-    c.execute("SELECT * FROM states WHERE name = (%s) ORDER BY id ASC",
-              (state,))
+    c.execute("SELECT * FROM states WHERE name = '{}' \
+        ORDER BY id ASC".format(argv[4]))
     records = c.fetchall()
     for record in records:
         print(record)
