589f4c854f3e6a78d4c686bd04da6c2f19da1ccf
nathanhall762 <4943@holbertonstudents.com>
2022-10-18 12:43:55 -0500

    fix: add argv

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ documentation """
 import MySQLdb
+from sys import argv
 
 if __name__ == '__main__':
     """documentation"""
-    db = MySQLdb.connect(user="root", passwd="usaUSA777", db="hbtn_0e_0_usa")
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM states ORDER BY id ASC")
     records = c.fetchall()
