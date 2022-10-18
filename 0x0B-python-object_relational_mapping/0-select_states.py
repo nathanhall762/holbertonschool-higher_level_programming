f87b220f03c323ac049f810dece62eba2cea2cfd
nathanhall762 <4943@holbertonstudents.com>
2022-10-18 12:40:24 -0500

    fix: not sure how

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     """documentation"""
     db = MySQLdb.connect(user="root", passwd="usaUSA777", db="hbtn_0e_0_usa")
     c = db.cursor()
-    c.execute("""SELECT id name FROM states ORDER BY id ASC""")
+    c.execute("SELECT id name FROM states ORDER BY id ASC")
     records = c.fetchall()
     for record in records:
         print(record)
