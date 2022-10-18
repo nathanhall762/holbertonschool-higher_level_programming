3beefb09c45ed06fcc92be956cde69350494a545
nathanhall762 <4943@holbertonstudents.com>
2022-10-18 13:03:27 -0500

    style: line too long

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
     state = argv[4]
-    c.execute("SELECT * FROM states WHERE name = (%s) ORDER BY id ASC", (state,))
+    c.execute("SELECT * FROM states WHERE name = (%s) ORDER BY id ASC",
+              (state,))
     records = c.fetchall()
     for record in records:
         print(record)
