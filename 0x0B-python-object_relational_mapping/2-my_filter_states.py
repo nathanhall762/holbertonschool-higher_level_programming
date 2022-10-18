6e67fb9ec284e07d81801031656f2224a5c9b5c9
nathanhall762 <4943@holbertonstudents.com>
2022-10-18 13:02:44 -0500

    init 2:

---
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" documentation """
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """documentation"""
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    state = argv[4]
+    c.execute("SELECT * FROM states WHERE name = (%s) ORDER BY id ASC", (state,))
+    records = c.fetchall()
+    for record in records:
+        print(record)
