985b350ceef339d4c9fadd539b58f6003476412a
nathanhall762 <4943@holbertonstudents.com>
2022-10-18 12:37:57 -0500

    style 0: added documentation

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" documentation """
 import MySQLdb
 
 if __name__ == '__main__':
+    """documentation"""
     db = MySQLdb.connect(user="root", passwd="usaUSA777", db="hbtn_0e_0_usa")
     c = db.cursor()
     c.execute("""SELECT id name FROM states ORDER BY id ASC""")
