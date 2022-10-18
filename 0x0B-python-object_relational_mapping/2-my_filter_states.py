39cb1484be9e5c487d0ce66fbfe30a6bcaa04e8b
nathanhall762 <4943@holbertonstudents.com>
2022-10-18 13:13:07 -0500

    try 2: like binary

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
     state = argv[4]
-    c.execute("SELECT * FROM states WHERE name = '{}' \
+    c.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
         ORDER BY id ASC".format(argv[4]))
     records = c.fetchall()
     for record in records:
