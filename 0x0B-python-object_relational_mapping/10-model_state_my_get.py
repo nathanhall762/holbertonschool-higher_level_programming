98f4c31b7cec793ce046759736fee7d9b3aac75b
nathanhall762 <4943@holbertonstudents.com>
2022-10-20 14:50:08 -0500

    init 10:

---
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""documentation"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """documentation"""
+    Session = sessionmaker(bind=create_engine
+                           ('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
+                            (argv[1], argv[2], argv[3])))
+
+    session = Session()
+
+    instance = session.query(State).filter(State.name == argv[4]).first()
+    if instance is None:
+        print('Nothing')
+    else:
+        print('{}'.format(instance.id))
