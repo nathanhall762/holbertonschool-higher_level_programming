54623676e1f317ed44eaea6e2ed6bde2e99bc535
nathanhall762 <4943@holbertonstudents.com>
2022-10-20 10:14:49 -0500

    init 7:

---
+++ 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""documentation"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1],
+                                                          argv[2],
+                                                          argv[3])
+    engine = create_engine(db)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    for instance in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(instance.id, instance.name))
