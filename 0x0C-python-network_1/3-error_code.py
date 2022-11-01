c84e87249ebc3d8f8a5f57eba4c91360c9c7fee0
nathanhall762 <4943@holbertonstudents.com>
2022-11-01 17:17:53 -0500

    init 3

---
+++ 0x0C-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""documentation"""
+from sys import argv
+from urllib.error import HTTPError
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+if __name__ == "__main__":
+    url = argv[1]
+
+    request = Request(url)
+
+    try:
+        with urlopen(request) as response:
+            print(response.read().decode("utf-8"))
+    except HTTPError as ex:
+        print('Error Code:', ex.code)
