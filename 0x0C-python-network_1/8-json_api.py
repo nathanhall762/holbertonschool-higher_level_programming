68da0c5b343b14dbdd0e09b02a274004e440b9e6
nathanhall762 <4943@holbertonstudents.com>
2022-11-01 20:36:38 -0500

    init 8

---
+++ 0x0C-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""documentation"""
+from sys import argv
+import requests
+
+
+if __name__ == "_main__":
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ''
+
+    try:
+        url = 'http://0.0.0.0:5000/search_user'
+        data = {'q': q}
+        r = requests.post(url, data).json()
+
+        if {'id', 'name'} <= r.keys():
+            print('[{id}] {name}'.format(id=r.get('id'), name=r.get('name')))
+        else:
+            print('No result')
+    except ValueError:
+        print('Not a valid JSON')
