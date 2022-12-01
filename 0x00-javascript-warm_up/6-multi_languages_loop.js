7086557e0570d632b814a2db4c9017ee9358976b
nathanhall762 <4943@holbertonstudents.com>
2022-12-01 15:11:24 -0600

    task 6

---
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let step = 0; step < 3; step++) {
+  // Runs 5 times, with values of step 0 through 4.
+  console.log(messages[step]);
+}
