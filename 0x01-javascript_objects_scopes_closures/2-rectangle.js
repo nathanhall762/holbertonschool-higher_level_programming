a57df447ab6a0ac93292f6ae964aafdcb05980fb
nathanhall762 <4943@holbertonstudents.com>
2022-12-02 11:22:27 -0600

    semistandard fix and 4

--- 0x01-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-	constructor (width, height) {
-		if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
-			this.width = width;
-			this.height = height;
-		}
-	}
+  constructor (width, height) {
+    if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
 };
--- 0x01-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,21 +1,31 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-	constructor (width, height) {
-		if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
-			this.width = width;
-			this.height = height;
-		}
-	}
-	print () {
-		for (let i = 0; i < this.height; ++1) {
-			let j =0;
-			
-			for (; j < this.width; ++j) {
-				ProcessingInstruction.stdout.write('X');
-			}
-			if (j === this.width) {
-				console.log('');
-			}
-		}
-	}
-}
+  constructor (width, height) {
+    if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; ++i) {
+      let j = 0;
+
+      for (; j < this.width; ++j) {
+        process.stdout.write('X');
+      }
+      if (j === this.width) {
+        console.log('');
+      }
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
