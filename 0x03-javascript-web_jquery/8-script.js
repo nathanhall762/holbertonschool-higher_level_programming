3a8ba6605ede1ce631ae5aa422b27206386d65e4
nathanhall762 <4943@holbertonstudents.com>
2022-12-07 14:39:16 -0600

    init all

---
+++ 0x03-javascript-web_jquery/8-script.js
@@ -0,0 +1,12 @@
+const $ = window.$;
+$.ajax({
+  url: 'https://swapi-api.hbtn.io/api/films/?format=json',
+  type: 'GET',
+  dataType: 'json'
+})
+  .done(function (json) {
+    const titles = json.results;
+    for (let i = 0; i < titles.length; i++) {
+      $('UL#list_movies').append('<li>' + titles[i].title + '</li>');
+    }
+  });
---
+++ 0x03-javascript-web_jquery/9-script.js
@@ -0,0 +1,9 @@
+const $ = window.$;
+$.ajax({
+  url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+  type: 'GET',
+  dataType: 'json'
+})
+  .done(function (json) {
+    $('DIV#hello').text(json.hello);
+  });
